73bce6d52224daf1724a8f56c7a4f9451a1a9962
Neel Iyer <neel.r.iyer@gmail.com>
2019-09-29 14:38:48 +1000

    updated train/test

--- retinanet/training_test_split.py
+++ retinanet/training_test_split.py
@@ -10,14 +10,14 @@ path = os.getcwd()
 df = pd.read_csv(path+'/processed_JSON/retinanet_data.csv')
 
 
-#df['split'] = np.random.randn(df.shape[0], 1)
+df['split'] = np.random.randn(df.shape[0], 1)
 
-#msk = np.random.rand(len(df)) <= 0.7
+msk = np.random.rand(len(df)) <= 0.99
 
-#train = df[msk]
-#test = df[~msk]
+train = df[msk]
+test = df[~msk]
 
-train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+#train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
 
 
 if os.path.exists(os.getcwd()+'/train_annotations.csv'):
@@ -25,14 +25,14 @@ if os.path.exists(os.getcwd()+'/train_annotations.csv'):
 
 if os.path.exists(os.getcwd()+'/test_annotations.csv'):
 	os.remove(os.getcwd()+'/test_annotations.csv')
-
+"""
 if os.path.exists(os.getcwd()+'/val_annotations.csv'):
 	os.remove(os.getcwd()+'/val_annotations.csv')
 
-
+"""
 train.to_csv(os.getcwd()+'/train_annotations.csv', header=True, index=None, sep=',', mode='a')
 test.to_csv(os.getcwd()+'/test_annotations.csv', header=True, index=None, sep=',', mode='a')
-validate.to_csv(os.getcwd()+'/val_annotations.csv', header=True, index=None, sep=',', mode='a')
+#validate.to_csv(os.getcwd()+'/val_annotations.csv', header=True, index=None, sep=',', mode='a')
 
 
 
