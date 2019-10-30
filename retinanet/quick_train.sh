311997e1532e41e9e2a875356c57f6598b4377d5
Neel Iyer <33139997+spiyer99@users.noreply.github.com>
2019-10-30 11:31:08 +1100

    more changes to pipeline due to new data

--- retinanet/quick_train.sh
+++ retinanet/quick_train.sh
@@ -6,8 +6,10 @@ python supervisely_to_csv.py
 echo "adding spacenet data"
 python tf_to_retinanet.py
 
-echo "building training and test csv: training_test_split.py"
+echo "building training and test csv"
 python training_test_split.py
+
+echo "making changes to new data"
 python spacenet_train_test_split.py
 python delete_row.py
 
